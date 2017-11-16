375c8fbce377cc1292930cc54c06e09a7caf9e16
Dylan Smyth <dylan@admin-dell.admin-dell>
2017-11-16 14:59:09 +0000

    Added human readable printing to onos-websocket

--- modules/onos_websocket.py
+++ modules/onos_websocket.py
@@ -4,6 +4,7 @@ import websocket
 import json
 from threading import Thread
 import modules.sdnpwn_common as sdnpwn
+import tabulate
 
 updatePrefsReq = '{"event":"updatePrefReq","payload":{"key":"topo_prefs","value":{"insts":1,"summary":1,"detail":1,"hosts":0,"offdev":1,"dlbls":0,"porthl":1,"bg":0,"spr":0,"ovid":"traffic","toolbar":0}}}'
 requestSummary = '{"event":"requestSummary","payload":{}}'
@@ -25,6 +26,8 @@ def usage():
   sdnpwn.addUsage(["-c", "--cookie"], "Add cookie (like 'JSESSIONID=1sz99uvm1z2971t18f55lmpc0d')", False)
   sdnpwn.addUsage(["-s", "--summary"], "Request Summary events", False)
   sdnpwn.addUsage(["-d", "--topo"], "Request Topology events", False)
+  sdnpwn.addUsage(["-b", "--pretty"], "Print event information in a human readable format", False)
+  
   #sdnpwn.addUsage(["-k", "--keep-alive"], "Keep websocket open and receive new events", False)
   
   return sdnpwn.getUsage()
@@ -36,8 +39,11 @@ def onOpen(ws):
 
 def onMessage(ws, msgJSON):
   msg = json.loads(msgJSON)
-  print(json.dumps(msg, indent=4, sort_keys=True))
-  ws.sdnpwn_expected_events -= 1
+  if(sdnpwn.checkArg(["-b", "--pretty"], ws.sdnpwn_params)):
+    parseMessage(msg)
+  else:
+    print(json.dumps(msg, indent=4, sort_keys=True))
+    ws.sdnpwn_expected_events -= 1
   
 def onError(ws, err):
   sdnpwn.printError("Got error: " + str(err))
@@ -57,6 +63,61 @@ def requestEvents(ws):
       #pass
     #sdnpwn.printNormal("Closing Websocket")
     #ws.close()
+
+def parseMessage(msg):
+  event = msg['event']
+  eventHandler = {
+                  'showSummary': parseSummary,
+                  'bootstrap': parseBootstrap,
+                  'addDevice': parseDevice,
+                  'addHost': parseHost,
+                  'topoStartDone': (lambda m: m)
+                  }
+  eventHandler[event](msg)
+  
+def parseBootstrap(msg):
+  user = msg['payload']['user']
+  print("User: " + msg['payload']['user'])
+  print("Cluster Nodes: " + str(len(msg['payload']['clusterNodes'])))
+  for c in msg['payload']['clusterNodes']:
+    print("    ID: " + c['id'])
+    print("    IP: " + c['ip'])
+    print("    Attached: " + str(c['m_uiAttached']))
+    print("")
+  #print(payload)
+  
+def parseSummary(msg):
+  print(msg['payload']['title'] + " Update:")
+  print("      Version: " + msg['payload']['props']['Version'])
+  print("      Devices: " + msg['payload']['props']['Devices'])
+  print("      Hosts: " + msg['payload']['props']['Hosts'])
+  print("      Links: " + msg['payload']['props']['Links'])
+  print("      Intents: " + msg['payload']['props']['Intents'])
+  print("      Tunnels: " + msg['payload']['props']['Tunnels'])
+  print("      Topology SCCs: " + msg['payload']['props']['Topology SCCs'])
+  #print(msg)
+  
+def parseDevice(msg):
+  print("New Device:")
+  print("      Type: " + msg['payload']['type'])
+  print("      ID: " + msg['payload']['id'])
+  print("      Master: " + msg['payload']['master'])
+  print("      Online: " + str(msg['payload']['online']))
+  print("      Channel ID: " + msg['payload']['props']['channelId'])
+  print("      IP Address: " + msg['payload']['props']['managementAddress'])
+  print("      Protocol: " + msg['payload']['props']['protocol'])
+  print("      Labels: " + str(msg['payload']['labels']))
+  
+def parseHost(msg):
+  print("New Host:")
+  print("      Type: " + msg['payload']['type'])
+  print("      ID: " + msg['payload']['id'])
+  print("      Labels: " + str(msg['payload']['labels']))
+  print("      Connection Point: ")
+  print("            Device: " + msg['payload']['cp']['device'])
+  print("            Port: " + str(msg['payload']['cp']['port']))
+  print("      Ingress: " + msg['payload']['ingress'])
+  print("      Engress: " + msg['payload']['egress'])
 
 def run(params):
   signal.signal(signal.SIGINT, signal_handler) #Assign the signal handler
