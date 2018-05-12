c6789b5a536c9122cbb65b740115452895c217e1
Dylan Smyth <dylan@admin-dell.admin-dell>
2018-05-12 11:01:25 +0100

    Made Maven optional in setup

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ From package manager:
    - python3-scipy
    - bridge-utils
    - python3-pip
-   - maven
 From pip3:
    - scapy-python
    - python-openflow
@@ -23,10 +22,21 @@ read -p "Install Required Software? [y/N] " res
 
 if [ "$res" == "y" ]; then
   apt-get update
-  apt-get install python3 python3-pip python3-netifaces python3-scipy bridge-utils python3-tabulate maven
+  apt-get install python3 python3-pip python3-netifaces python3-scipy bridge-utils python3-tabulate
   pip3 install scapy-python3
   pip3 install python-openflow
   pip3 install websocket-client
+
+echo -e "\n
+
+Maven is used by some modules to build Java-based SDN applications"
+
+  read -p "Would you like to install Maven? [y/N] " res
+
+  if [ "$res" == "y" ]; then
+    apt-get install maven
+  fi
+
 else
   echo "Quiting."
 fi
