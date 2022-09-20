ce572272a91c1646144b3e5bb00d849387761f7b
smythtech <dylan@smythtech.net>
2022-09-20 10:04:36 +0100

    Fixed Scapy install issue. Cleaned up setup.sh

--- setup.sh
+++ setup.sh
@@ -11,8 +11,8 @@ From package manager:
    - python3-tabulate
    - python3-pip
    - git
-From pip3:
-   - scapy-python
+From pip:
+   - scapy
    - websocket-client
 From github:
    - python-openflow-legacy (Older version of Kytos OpenFlow library)
@@ -25,11 +25,12 @@ NOTE: sdnpwn works best with Python versions 3.4 and 3.5. Some modules may not f
 read -p "Install Required Software? [y/N] " res
 
 if [ "$res" == "y" ]; then
-  apt-get update
-  apt-get install python3 python3-pip python3-netifaces python3-scipy git bridge-utils python3-tabulate
-  pip3 install scapy-python3
+  sudo apt-get update
+  sudo apt-get install python3 python3-pip python3-netifaces python3-scipy git bridge-utils python3-tabulate
+  pip install scapy
+  sudo pip install scapy # For whatever reason we need to install this explicitly for root. Must look into this later.
   #pip3 install python-openflow
-  pip3 install websocket-client
+  pip install websocket-client
   
   mkdir lib
   cd lib
